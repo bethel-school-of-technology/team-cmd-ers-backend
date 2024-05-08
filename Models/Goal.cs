6adc42c9bab4e1d1183d5943c3afb3c5a6d1e7c2
Ryan Dorrell <rdorrell1997@gmail.com>
2024-05-08 17:37:20 -0500

    Created User and Goal Models

---
+++ Models/Goal.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Fit_Trac.Models;
+
+public class Goal
+{
+    public int Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? Type { get; set; }
+    [Required]
+    public int GoalToReach { get; set; }
+    public string DateCreated { get; set; }
+    public int UserId { get; set; }
+    public virtual User? User { get; set; }
+
+    public Goal()
+    {
+        this.DateCreated = DateTime.Now.ToString("MM-dd-yyyy");
+    }
+    
+}
