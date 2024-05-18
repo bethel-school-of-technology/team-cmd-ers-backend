35bcd511ad8eb7c17bea3a435d665c7c6d828af6
Ryan Dorrell <rdorrell1997@gmail.com>
2024-05-17 23:15:30 -0500

    Added Swagger UI Authentication and Authentication Options and config

--- Program.cs
+++ Program.cs
@@ -1,18 +1,64 @@
 using Fit_Trac.Migrations;
 using Fit_Trac.Repositories;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var secretKey = builder.Configuration["TokenSecret"];
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fit_Trac", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+        In = ParameterLocation.Header, 
+        Description = "Please insert Bearer followed by JWT",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey 
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    { 
+        new OpenApiSecurityScheme 
+        { 
+        Reference = new OpenApiReference 
+        { 
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer" 
+        } 
+        },
+        new string[] { } 
+        } 
+    });
+});
 
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IGoalRepository, GoalRepository>();
 builder.Services.AddSqlite<GoalDbContext>("Data Source=Fit_Trac.db");
+
+builder.Services.AddAuthentication(options => 
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(config => 
+{
+    config.RequireHttpsMetadata = true;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ValidateLifetime = false,
+        RequireExpirationTime = false,
+        ClockSkew = TimeSpan.Zero,
+        ValidateIssuerSigningKey = true
+    };
+});
 
 var app = builder.Build();
 
