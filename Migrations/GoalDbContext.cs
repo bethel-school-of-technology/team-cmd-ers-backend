8a0e6d2e49b090ffc9033160a2a98b6caa6d689f
Ryan Dorrell <rdorrell1997@gmail.com>
2024-06-02 02:21:00 -0500

    Added New table to our Db and GoalDBContext files for our daily input

--- Migrations/GoalDbContext.cs
+++ Migrations/GoalDbContext.cs
@@ -7,6 +7,7 @@ public class GoalDbContext : DbContext
 {
     public DbSet<Goal> Goal { get; set; }
     public DbSet<User> User { get; set; }
+    public DbSet<DailyGoalInput> DailyGoalInputs { get; set; }
 
     public GoalDbContext(DbContextOptions<GoalDbContext> options)
         : base(options)
@@ -24,12 +25,16 @@ public class GoalDbContext : DbContext
             entity.Property(e => e.Type).IsRequired();
             entity.Property(e => e.Description);
             entity.Property(e => e.GoalToReach).IsRequired();
-            entity.Property(e => e.UserProgress).IsRequired();
             entity.Property(e => e.DateCreated).IsRequired();
 
             entity.HasOne<User>(e => e.User)
                 .WithMany(u => u.Goal)
                 .HasForeignKey(e => e.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+            
+            entity.HasMany<DailyGoalInput>(e => e.DailyGoalInput)
+                .WithOne(d => d.Goal)
+                .HasForeignKey(d => d.GoalId)
                 .OnDelete(DeleteBehavior.Cascade);
         });
 
@@ -44,6 +49,18 @@ public class GoalDbContext : DbContext
             entity.HasMany<Goal>(e => e.Goal)
                 .WithOne(g => g.User)
                 .HasForeignKey(g => g.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<DailyGoalInput>(entity =>
+        {
+            entity.HasKey(e => e.InputId);
+            entity.Property(e => e.ProgressInput).IsRequired();
+            entity.Property(e => e.Date).IsRequired();
+            
+            entity.HasOne<Goal>(e => e.Goal)
+                .WithMany(g => g.DailyGoalInput)
+                .HasForeignKey(g => g.GoalId)
                 .OnDelete(DeleteBehavior.Cascade);
         });
     }
